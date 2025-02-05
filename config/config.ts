1f0850da654a2daa250a398f26c1a44a7eb116b2
marco0antonio0 <marcomesquitajr@hotmail.com>
2025-02-04 23:56:41 -0300

    feat/dirrocha-cms

--- config/config.ts
+++ config/config.ts
@@ -1,10 +1,10 @@
 // 🔹 firebaseConfig.ts
 export const IsStartedfirebaseConfig = true;
 export const firebaseConfig = {
-  "apiKey": "AIzaSyAEuiJWG1NSpLXy4WCEwHN5EaMCarbISQA",
-  "authDomain": "cms-demo-cb7fc.firebaseapp.com",
-  "projectId": "cms-demo-cb7fc",
-  "storageBucket": "cms-demo-cb7fc.firebasestorage.app",
-  "messagingSenderId": "743128664817",
-  "appId": "1:743128664817:web:559617db6ad9fc753a3750"
+  "apiKey": "",
+  "authDomain": "",
+  "projectId": "",
+  "storageBucket": "",
+  "messagingSenderId": "",
+  "appId": ""
 };
