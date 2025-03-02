b5d7512608096957ce8b000e7df913300dbe4670
marco0antonio0 <marcomesquitajr@hotmail.com>
2025-03-02 12:14:29 -0300

    feat/DirrochaCMS-38: fix melhoria de seguranca no endpoint login interno

--- pages/api/login.ts
+++ pages/api/login.ts
@@ -5,6 +5,14 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
   if (req.method !== 'POST') {
     return res.status(405).json({ message: 'Method not allowed' });
   }
+
+  // 🔒 Verifica se a chamada vem do próprio servidor Next.js
+  const host = req.headers.host || "";
+  const referer = req.headers.referer || "";
+
+  if (!referer.includes(host)) {
+    return res.status(403).json({ error: "Acesso negado" });
+  }
   try {
     const { name, password } = req.body;
     const token = await loginUser(name, password);
